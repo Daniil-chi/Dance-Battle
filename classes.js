cb74460e1c1ffbe20474b6a94997ed28bacce13f
Daniil-chi <149510726+Daniil-chi@users.noreply.github.com>
2024-01-14 04:24:10 +0300

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,51 @@
+class Hero {
+    constructor(name, level, healthPoints, stats) {
+        this.name = name;
+        this.level = level;
+        this.healthPoints = healthPoints;
+        this.stats = stats;
+    };
+
+    displayHero() {
+        const heroInfo =
+            `Имя: ${this.name}` +
+            `\nУровень: ${this.level}` +
+            `\nЖизненные силы: ${this.healthPoints}` +
+            `\nСила: ${this.stats.str}` +
+            `\nИнтеллект: ${this.stats.int}` +
+            `\nЛовкость: ${this.stats.agi}`;
+        console.log(heroInfo);
+    };
+};
+
+class Knight extends Hero {
+    constructor(name, level, healthPoints, stats, isHorseTango, energy) {
+        super(name, level, healthPoints, stats);
+        this.isHorseTango = isHorseTango;
+        this.energy = energy;
+    };
+    displayHero() {
+        super.displayHero();
+        console.log(`Энергия: ${this.energy}`)
+
+        if (this.isHorseTango === "true") {
+          console.log("Этот герой может танцевать танго на коне")
+    }
+}
+};
+
+class Mage extends Hero {
+    constructor(name, level, healthPoints, stats, hasTectonicPotion, mana) {
+        super(name, level, healthPoints, stats);
+        this.hasTectonicPotion = hasTectonicPotion;
+        this.mana = mana;
+    };
+
+    displayHero() {
+        super.displayHero();
+        console.log(`Мана: ${this.mana}`);
+        if (this.hasTectonicPotion === "true") {
+            console.log("Моя фляга полна сил маны")
+        }
+    };
+};
